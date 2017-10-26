a60b7703b6eee0d12dcf0fdff225990e604ea1f1
Roberto C. Torres <roberto@UIMEIP-PowerEdge>
2017-10-26 15:46:15 -0500

    Load files into array of array

--- ClasificadorBayes.pl
+++ ClasificadorBayes.pl
@@ -1,57 +1,99 @@
 #!/usr/bin/perl -w
 use strict;
-use lib "/home/roberto/lib";
+use List::MoreUtils qw(uniq);
+use lib "/home/rtorres/lib";
 use Routines;
 
-my($BoleanTable, $MainPath, $Row, $MetaData, $StrainData, $StrainRegion,
-   $A, $B, $C, $D, $nMetaData, $N, $pA, $pB, $pC, $pD, $pNoA, $pNoB, $pNoC, $pNoD);
-my($i);
-my(@BoleanTable,@MetaData, @StrainData);
+my($MainPath, $BoleanFileName, $MetaDataFileName, $nBoleanFile, $Line,
+   $nBoleanFileFields, $MetaData, $nMetaDataFile, $nMetaDataFileFields,
+   $Region, $Class);
+my($i, $j);
+my(@BoleanFile, @BoleanFileFields, @BoleanTable, @MetaDataField, @MetaDataFile,
+   @MetaDataFileFields, @MetaData, @Classes);
+my(%Classes);
+my $BoleanTable = [ ]; 
 
-$MainPath = "/home/roberto/NaiveBayes";
-$BoleanTable = $MainPath ."/". "Tabla.csv";
-$MetaData = $MainPath ."/". "MetaData.csv";
+$MainPath = "/home/rtorres/Bayes";
+$BoleanFileName = $MainPath ."/". "Tabla.csv";
+$MetaDataFileName = $MainPath ."/". 'MetaData.csv';
 
-@BoleanTable = ReadFile($BoleanTable);
-@MetaData = ReadFile($MetaData);
-$nBoleanTable = scalar@nBoleanTable;
-$nMetaData = scalar@MetaData;
-$N = $nMetaData-1;
+@BoleanFile = ReadFile($BoleanFileName);
+$nBoleanFile = scalar@BoleanFile;
 
-for($i=1;$i<$nMetaData;$i++){
-	$Row = $MetaData[$i];
-	@StrainData = split(',',$Row);
-	$StrainRegion = $StrainData[1];
-	if($StrainRegion eq 'A'){
-		$A++;
-	}elsif($StrainRegion eq 'B'){
-		$B++;
-	}elsif($StrainRegion eq 'C'){
-		$C++;
-	}elsif($StrainRegion eq 'D'){
-		$D++;
-	}
+for ($i=0; $i<$nBoleanFile; $i++){
+	$Line = $BoleanFile[$i];
+	@BoleanFileFields = split(",",$Line);
+	$nBoleanFileFields = scalar@BoleanFileFields;
+	push (@BoleanTable, [@BoleanFileFields]);
 }
 
-push $A, @nClass;
+@MetaDataFile = ReadFile($MetaDataFileName);
+$nMetaDataFile = scalar@MetaDataFile;
 
+for ($i=0; $i<$nMetaDataFile; $i++){
+	$Line = $MetaDataFile[$i];
+	@MetaDataFileFields = split(",",$Line);
+	$nMetaDataFileFields = scalar@MetaDataFileFields;
+	push (@MetaData, [@MetaDataFileFields]);
+}
 
-$pA = $A/$N;
-$pB = $B/$N;
-$pC = $C/$N;
-$pD = $D/$N;
-$pNoA = 1-$pA;
-$pNoB = 1-$pB;
-$pNoC = 1-$pC;
-$pNoD = 1-$pD;
+for ($i=1;$i<$nMetaDataFileFields;$i++){
+	$Class = $MetaData[$i]->[1];
+	push @Classes, $Class;
+}
 
+@Classes = uniq(@Classes);
 
-for($i=1;$i<$nBoleanTable;$i++){
-	$Probe = $BoleanTable[$i];
-	@ProbeData = split(',',$Probe);
-	for($j=1;$j<
-
-print "\nVar A:$A\nVar B:$B\nVar C:$C\nVar D:$D\nTotal: $N\n$nMetaData";
-
+foreach my $Element(@Classes){
+	print "\n$Element\n";
+}
 exit;
-		
+
+#
+#@Classes = ("A","B","C","D");
+#@BoleanTable = ReadFile($BoleanTable);
+#@MetaData = ReadFile($MetaData);
+#$nBoleanTable = scalar@BoleanTable;
+#$nMetaData = scalar@MetaData;
+#$N = $nMetaData-1;
+#
+#
+#
+#for($i=1;$i<$nMetaData;$i++){
+#	$Row = $MetaData[$i];
+#	@StrainData = split(',',$Row);
+#	$StrainRegion = $StrainData[1];
+#	
+#	foreach $Class(@Classes){
+#		if($StrainRegion eq $Class){
+#		$Classes{$Class} = $Counter++;
+#	}
+#}
+#
+#$pA = $A/$N;
+#$pB = $B/$N;
+#$pC = $C/$N;
+#$pD = $D/$N;
+#exit;
+#$pNoA = 1-$pA;
+#$pNoB = 1-$pB;
+#$pNoC = 1-$pC;
+#$pNoD = 1-$pD;
+#
+#for($i=1;$i<$nBoleanTable;$i++){
+#	$Probe = $BoleanTable[$i];
+#	@ProbeData = split(',',$Probe);
+#	for($j=1;$j<$N+1;$j++){
+#		$Hit = $ProbeData[$j];
+#		foreach $Class (@Classes){
+#		
+#		if($Hit == "1"){
+#			$TotalProbe++;
+#		}
+#	}
+#}
+#
+#print "\nVar A:$A\nVar B:$B\nVar C:$C\nVar D:$D\nTotal Columns: $N\nTotal Probes: $TotalProbe\n";
+#
+#exit;
+#		
