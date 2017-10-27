0bec5e8b52ed966d6d77cb77c7c05454a73a1247
Roberto C. Torres <torres.roberto.c@gmail.com>
2017-10-27 02:49:18 -0500

    Use of hashes to load all classes information and compute probabilities

--- ClasificadorBayes.pl
+++ ClasificadorBayes.pl
@@ -1,99 +1,97 @@
 #!/usr/bin/perl -w
 use strict;
 use List::MoreUtils qw(uniq);
-use lib "/home/rtorres/lib";
+use lib "/Users/rc/lib";
 use Routines;
 
 my($MainPath, $BoleanFileName, $MetaDataFileName, $nBoleanFile, $Line,
-   $nBoleanFileFields, $MetaData, $nMetaDataFile, $nMetaDataFileFields,
-   $Region, $Class);
+   $nBoleanFileFields, $N, $MetaData, $nMetaDataFile, $nMetaDataFileFields,
+   $Region, $Strain, $Class, $nClasses, $Counter, $Hit, $Count);
 my($i, $j);
 my(@BoleanFile, @BoleanFileFields, @BoleanTable, @MetaDataField, @MetaDataFile,
-   @MetaDataFileFields, @MetaData, @Classes);
-my(%Classes);
+   @MetaDataFileFields, @MetaData, @Classes, @Strains);
+my(%StrainClass, %Classes, %pClasses, %cpClasses, %ClassHits);
 my $BoleanTable = [ ]; 
 
-$MainPath = "/home/rtorres/Bayes";
+$MainPath = "/Users/rc/Bayes";
 $BoleanFileName = $MainPath ."/". "Tabla.csv";
 $MetaDataFileName = $MainPath ."/". 'MetaData.csv';
 
 @BoleanFile = ReadFile($BoleanFileName);
 $nBoleanFile = scalar@BoleanFile;
-
 for ($i=0; $i<$nBoleanFile; $i++){
 	$Line = $BoleanFile[$i];
 	@BoleanFileFields = split(",",$Line);
-	$nBoleanFileFields = scalar@BoleanFileFields;
 	push (@BoleanTable, [@BoleanFileFields]);
 }
+$nBoleanFileFields = scalar@BoleanFileFields;
+$N = $nBoleanFileFields-1;
 
 @MetaDataFile = ReadFile($MetaDataFileName);
 $nMetaDataFile = scalar@MetaDataFile;
-
 for ($i=0; $i<$nMetaDataFile; $i++){
 	$Line = $MetaDataFile[$i];
 	@MetaDataFileFields = split(",",$Line);
-	$nMetaDataFileFields = scalar@MetaDataFileFields;
 	push (@MetaData, [@MetaDataFileFields]);
 }
+$nMetaDataFileFields = scalar@MetaDataFileFields;
 
-for ($i=1;$i<$nMetaDataFileFields;$i++){
+for ($i=1;$i<$nMetaDataFile;$i++){
 	$Class = $MetaData[$i]->[1];
 	push @Classes, $Class;
 }
 
 @Classes = uniq(@Classes);
+$nClasses = scalar@Classes;
 
-foreach my $Element(@Classes){
-	print "\n$Element\n";
+for ($i=0;$i<$nClasses;$i++){
+	$Counter = 0;
+	for ($j=1;$j<$nMetaDataFile;$j++){
+		$Strain = $MetaData[$j]->[0];
+		$Class = $MetaData[$j]->[1];
+		push @Strains, $Strain;
+		$StrainClass{$Strain} = $Class;
+		if($Class eq $Classes[$i]){
+			$Counter++;
+		}
+	}
+	$Classes{$Classes[$i]} = $Counter;
+	$pClasses{$Classes[$i]} = $Counter/$N;
+	$cpClasses{$Classes[$i]} = 1-$Counter/$N;
 }
-exit;
 
-#
-#@Classes = ("A","B","C","D");
-#@BoleanTable = ReadFile($BoleanTable);
-#@MetaData = ReadFile($MetaData);
-#$nBoleanTable = scalar@BoleanTable;
-#$nMetaData = scalar@MetaData;
-#$N = $nMetaData-1;
-#
-#
-#
-#for($i=1;$i<$nMetaData;$i++){
-#	$Row = $MetaData[$i];
-#	@StrainData = split(',',$Row);
-#	$StrainRegion = $StrainData[1];
-#	
-#	foreach $Class(@Classes){
-#		if($StrainRegion eq $Class){
-#		$Classes{$Class} = $Counter++;
-#	}
-#}
-#
-#$pA = $A/$N;
-#$pB = $B/$N;
-#$pC = $C/$N;
-#$pD = $D/$N;
-#exit;
-#$pNoA = 1-$pA;
-#$pNoB = 1-$pB;
-#$pNoC = 1-$pC;
-#$pNoD = 1-$pD;
-#
-#for($i=1;$i<$nBoleanTable;$i++){
-#	$Probe = $BoleanTable[$i];
-#	@ProbeData = split(',',$Probe);
-#	for($j=1;$j<$N+1;$j++){
-#		$Hit = $ProbeData[$j];
-#		foreach $Class (@Classes){
-#		
-#		if($Hit == "1"){
-#			$TotalProbe++;
-#		}
-#	}
-#}
-#
-#print "\nVar A:$A\nVar B:$B\nVar C:$C\nVar D:$D\nTotal Columns: $N\nTotal Probes: $TotalProbe\n";
-#
-#exit;
-#		
+foreach my $Class(@Classes){
+print "\nThe Class $Class have $Classes{$Class} elements, and its probability is $pClasses{$Class} while the complemented probability is $cpClasses{$Class}";
+}
+
+$Count = 0;
+for ($i=1; $i<$nBoleanFile; $i++){
+	for ($j=1; $j<$nBoleanFileFields; $j++){
+		$Hit = $BoleanTable[$i][$j];
+		if ($Hit != 0){
+			$Count++;
+		}
+	}
+}
+
+print "\nThe total of Hits into the bolean table are $Count";
+
+foreach $Class(@Classes){
+	$Count = 0;
+	for ($i=1;$i<$nBoleanFileFields; $i++){
+		$Strain = $BoleanTable[0][$i];
+		if ($StrainClass{$Strain} eq $Class){
+			for ($j=1;$j<$nBoleanFile;$j++){
+				$Hit = $BoleanTable[$j][$i];
+				$Count += $Hit;
+			}
+		}
+	}
+	$ClassHits{$Class} = $Count;
+}
+
+foreach my $Class(@Classes){
+print "\nThe Class $Class have $ClassHits{$Class} hits";
+}
+print "\n";
+exit;
