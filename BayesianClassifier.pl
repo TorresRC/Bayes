c2b272882e4e099740258218d8d01f9f30a26362
Laboratorio de Biotecnología y Bioinformática Genómica <LabBioyBioGen@server.local>
2017-10-27 14:57:46 -0500

    Test

--- BayesianClassifier.pl
+++ BayesianClassifier.pl
@@ -1,19 +1,20 @@
 #!/usr/bin/perl -w
 use strict;
 use List::MoreUtils qw(uniq);
-use lib "/Users/rc/lib";
+use lib "/Users/LabBioyBioGen/Bayes/lib";
 use Routines;
 
 my($MainPath, $BoleanFileName, $MetaDataFileName, $nBoleanFile, $Line,
    $nBoleanFileFields, $N, $MetaData, $nMetaDataFile, $nMetaDataFileFields,
-   $Region, $Strain, $Class, $nClasses, $Counter, $Hit, $Count);
+   $Region, $Strain, $Class, $nClasses, $Counter, $Hit, $Count, $Probe, $StrainHit,
+   $StrainHits, $ProbeHit, $ProbeHits);
 my($i, $j);
 my(@BoleanFile, @BoleanFileFields, @BoleanTable, @MetaDataField, @MetaDataFile,
    @MetaDataFileFields, @MetaData, @Classes, @Strains);
-my(%StrainClass, %Classes, %pClasses, %cpClasses, %ClassHits);
+my(%StrainClass, %Classes, %pClasses, %cpClasses, %ClassHits, %ProbeClass);
 my $BoleanTable = [ ]; 
 
-$MainPath = "/Users/rc/Bayes";
+$MainPath = "/Users/LabBioyBioGen/Bayes";
 $BoleanFileName = $MainPath ."/". "Tabla.csv";
 $MetaDataFileName = $MainPath ."/". 'MetaData.csv';
 
@@ -49,7 +50,6 @@ for ($i=0;$i<$nClasses;$i++){
 	for ($j=1;$j<$nMetaDataFile;$j++){
 		$Strain = $MetaData[$j]->[0];
 		$Class = $MetaData[$j]->[1];
-		push @Strains, $Strain;
 		$StrainClass{$Strain} = $Class;
 		if($Class eq $Classes[$i]){
 			$Counter++;
@@ -77,21 +77,42 @@ for ($i=1; $i<$nBoleanFile; $i++){
 print "\nThe total of Hits into the bolean table are $Count";
 
 foreach $Class(@Classes){
-	$Count = 0;
+	$StrainHits = 0;
+   
 	for ($i=1;$i<$nBoleanFileFields; $i++){
 		$Strain = $BoleanTable[0][$i];
+      $Probe = $BoleanTable[$i][0];
+      #$ClassProbe{$Probe} = $Class;
+      
 		if ($StrainClass{$Strain} eq $Class){
 			for ($j=1;$j<$nBoleanFile;$j++){
-				$Hit = $BoleanTable[$j][$i];
-				$Count += $Hit;
-			}
+				$StrainHit = $BoleanTable[$j][$i];
+				$StrainHits += $StrainHit;
+         }
+         $ProbeHits = 0;
+         for ($j=1; $j<$nBoleanFileFields;$j++){
+            $ProbeHit = $BoleanTable[$i][$j];
+            $ProbeHits += $ProbeHit;
+			}   
 		}
+      $ProbeClass{$Probe} = $ProbeHits;
+      print "\n $ProbeClass{$Probe}\n";
+      #exit;
 	}
-	$ClassHits{$Class} = $Count;
+	$ClassHits{$Class} = $StrainHits;
 }
 
 foreach my $Class(@Classes){
 print "\nThe Class $Class have $ClassHits{$Class} hits";
 }
+
+#foreach $Class(@Classes){
+#   for ($i=1; $i<$nBoleanFile; $i++){
+#      $Probe = $BoleanTable[$i][0];
+#      for ($j=1; $j<$Classes{$Class}; $j++){
+#         $Probes{$Class} += $BoleanTable[$i][$j];
+#      }
+#   }
+
 print "\n";
 exit;
