5f8fce589b1ddeea521c9b90302150ae6f2b1457
Roberto C. Torres <torres.roberto.c@gmail.com>
2017-11-11 20:22:37 -0600

    Clean lineas and Use of FinBin module

--- FeaturesMLE_Classifier.pl
+++ FeaturesMLE_Classifier.pl
@@ -1,35 +1,59 @@
 #!/usr/bin/perl -w
-#use strict;
+use strict;
 use List::MoreUtils qw(uniq);
-use Getopt::Long qw(GetOptions);
-Getopt::Long::Configure qw(gnu_getopt);
-use lib "/Users/bioinformatica/Documents/Bayes/lib";
+use FindBin;
+use lib "$FindBin::Bin/lib";
 use Routines;
+my $MainPath = "$FindBin::Bin";
 
-my($MainPath, $LinesOnTrainingFile, $Line, $ColumnsOnTrainingFile, $N, $MetaData,
-   $LinesOnMetaDataFile, $ColumnsOnMetaDataFile, $Strain, $Class, $nClasses,
-   $Counter, $Hit, $Feature, $StrainHit, $StrainHits, $FeatureHit, $FeatureHits,
-   $nFeature, $nQryFile, $nQryFileFields, $QryHit, $QryStrain, $PossibleClass,
-   $Column, $FeaturesMLE, $Plot, $RScript);
+my ($Usage, $TrainingFile, $MetadataFile, $OutPath, $Chi2, $MLE, $PsCounts);
+
+$Usage = "\nUSAGE\n  $FindBin::Script <Observed Data [Absolute Path]>
+                            <Metadata [Absolute Path]>
+                            <Output Path [Relative Path]>
+                            <Pseudo Counts Increase [Integer]>
+                            <ChiSquared Test [Bolean]>
+                            <Maximum Likelihood Estimation [Bolean]>\n\n";
+unless(@ARGV) {
+        print $Usage;
+        exit;
+}
+chomp @ARGV;
+$TrainingFile = $ARGV[0];
+$MetadataFile = $ARGV[1];
+$OutPath      = $ARGV[2];
+$PsCounts     = $ARGV[3];
+$MLE          = $ARGV[4];
+$Chi2         = $ARGV[5];
+
+
+my($Test, $TestReport, $Plot, $RScript, $LinesOnTrainingFile, $nFeature, $Line,
+   $ColumnsOnTrainingFile, $N, $LinesOnMetaDataFile, $ColumnsOnMetaDataFile,
+   $PossibleClass, $Column, $Class, $nClasses, $Element, $GlobalHits, $Hit,
+   $Feature, $iClass, $a, $b, $c, $d, $nConfusion);
 my($i, $j);
-my(@TrainingFile, @TrainingFileFields, @TrainingMatrix, @MetaDataField, @MetaDataFile,
-   @MetaDataFileFields, @MetaData, @Classes, @Strains, @QryFile, @QryFileFields,
-   @QryMatrix);
-my(%StrainClass, %Classes, %pClasses, %cpClasses, %ClassHits, %FeatureClass,
-   %pFeatureClass, %cpFeatureClass, %FeatureTotalHits, %cpQry, %pQry, %MLE);
+my(@TrainingFile, @TrainingFileFields, @TrainingMatrix, @MetaDataFile,
+   @MetaDataFileFields, @MetaDataMatrix, @Classes, @Elements);
+my(%ClassOfElement, %Elements, %pClass, %cpClass, %ClassHits, %HitsOfFeaturesInClass,
+   %TotalFeatureHits, %Test);
 my(%a, %b, %c, %d);
-my $TrainingMatrix = [ ];
 my $Report = [ ];
 
-$MainPath = "/Users/bioinformatica/Documents/Bayes";
-$TrainingFileName = $MainPath ."/". "Training.csv";
-$MetaDataFileName = $MainPath ."/". 'MetaData.csv';
-$FeaturesMLE = $MainPath ."/". "MLE.csv";
-$Plot            = $MainPath ."/". "MLE.pdf";
-$RScript         = $MainPath ."/". "MLEScript.R";
+if ($MLE == 1 && $Chi2 == 0){
+   $Test = "MaximumLikelihoodEstimation";
+}elsif ($MLE == 0 && $Chi2 == 1){
+   $Test = "ChiSquared";
+}else {
+   print "\nYou should select only one test option (--Chi2 or --MLE)\n\tProgram finished!\n\n";
+   exit;
+}
 
-#Loading the bolean training file
-@TrainingFile = ReadFile($TrainingFileName);
+$TestReport = $OutPath ."/". $Test . ".csv";
+$Plot       = $OutPath ."/". $Test . "_plot.pdf";
+$RScript    = $OutPath ."/". "Script.R";
+
+# Loading the bolean training file
+@TrainingFile = ReadFile($TrainingFile);
 $LinesOnTrainingFile = scalar@TrainingFile;
 $nFeature = $LinesOnTrainingFile-1;
 for ($i=0; $i<$LinesOnTrainingFile; $i++){
@@ -40,20 +64,20 @@ for ($i=0; $i<$LinesOnTrainingFile; $i++){
 $ColumnsOnTrainingFile = scalar@TrainingFileFields;
 $N = $ColumnsOnTrainingFile-1;
 
-#Loading the metadata file
-@MetaDataFile = ReadFile($MetaDataFileName);
+# Loading the metadata file
+@MetaDataFile = ReadFile($MetadataFile);
 $LinesOnMetaDataFile = scalar@MetaDataFile;
 for ($i=0; $i<$LinesOnMetaDataFile; $i++){
 	$Line = $MetaDataFile[$i];
 	@MetaDataFileFields = split(",",$Line);
-	push (@MetaData, [@MetaDataFileFields]);
+	push (@MetaDataMatrix, [@MetaDataFileFields]);
 }
 $ColumnsOnMetaDataFile = scalar@MetaDataFileFields;
 
-#Obtaining classes
+# Obtaining classes
 print "\nThe following columns were detected as possible classes:";
 for ($i=1;$i<$ColumnsOnMetaDataFile;$i++){
-        $PossibleClass = $MetaData[0][$i];
+        $PossibleClass = $MetaDataMatrix[0][$i];
         print "\n\t[$i] $PossibleClass";
 }
 print "\n\nPlease type the number of the desired class: ";
@@ -61,74 +85,65 @@ $Column = <STDIN>;
 chomp $Column;
 
 for ($i=1;$i<$LinesOnMetaDataFile;$i++){
-	$Class = $MetaData[$i]->[$Column];
+	$Class = $MetaDataMatrix[$i]->[$Column];
 	push @Classes, $Class;
 }
 @Classes = uniq(@Classes);
 $nClasses = scalar@Classes;
 
 for ($i=0;$i<$nClasses;$i++){
-	$Counter = 0;
 	for ($j=1;$j<$LinesOnMetaDataFile;$j++){
-		$Strain = $MetaData[$j]->[0];
-		$Class = $MetaData[$j]->[1];
-		$StrainClass{$Strain} = $Class;
+		$Element = $MetaDataMatrix[$j]->[0];
+		$Class = $MetaDataMatrix[$j]->[1];
+		$ClassOfElement{$Element} = $Class;
 		if($Class eq $Classes[$i]){
-			$Counter++;
+         $Elements{$Classes[$i]}++; #   <-------- Number of elements in each class
 		}
 	}
-	$Classes{$Classes[$i]} = $Counter; #Number of elements in each class
-	$pClasses{$Classes[$i]} = $Counter/$N; #Probability of each class
-	$cpClasses{$Classes[$i]} = 1-$Counter/$N; #Complement probability of each class
+	#$pClass{$Classes[$i]} = $Elements{$Classes[$i]}/$N; # Probability of each class
+	#$cpClass{$Classes[$i]} = 1-$Elements{$Classes[$i]}/$N; # Complement probability of each class
 }
 
-#Hits into the training matrix
+# Hits into the training matrix
 $GlobalHits = 0;
 for ($i=1; $i<$LinesOnTrainingFile; $i++){
 	for ($j=1; $j<$ColumnsOnTrainingFile; $j++){
 		$Hit = $TrainingMatrix[$i][$j];
 		if ($Hit != 0){
-			$GlobalHits++; # Total of hits
+			$GlobalHits++; #   <-------------------------------------- Total of hits
 		}
 	}
 }
 
 #Hits of each class
 foreach $Class(@Classes){
-	$StrainHits = 0; 
 	for ($i=1;$i<$ColumnsOnTrainingFile; $i++){
-		$Strain = $TrainingMatrix[0][$i];
-		if ($StrainClass{$Strain} eq $Class){
+		$Element = $TrainingMatrix[0][$i];
+		if ($ClassOfElement{$Element} eq $Class){
 			for ($j=1;$j<$LinesOnTrainingFile;$j++){
-				$StrainHit = $TrainingMatrix[$j][$i];       #  <----- Observed Hits
-            #$StrainHit = $TrainingMatrix[$j][$i]+1;    #  <----- Pseudo counts Hits
-				$StrainHits += $StrainHit;
+            $ClassHits{$Class} += $TrainingMatrix[$j][$i]+$PsCounts;  # <- Total of hits in class
 			}
 		}
 	}
-	$ClassHits{$Class} = $StrainHits; # Total of hits of each class
 }
 
-#Hits of each probe in each class
+#Hits of each feature in each class
 foreach $Class(@Classes){
 	for ($i=1;$i<$LinesOnTrainingFile;$i++){
 		$Feature = $TrainingMatrix[$i][0];
-      $FeatureTotalHits{$Feature} = 0;
+      $TotalFeatureHits{$Feature} = 0;
 		for ($j=1;$j<$ColumnsOnTrainingFile; $j++){         
-			$Strain = $TrainingMatrix[0][$j];
-         $FeatureTotalHits{$Feature} += $TrainingMatrix[$i][$j];   # <--- Observed Hits
-         #$FeatureTotalHits{$Feature} += $TrainingMatrix[$i][$j]+1;   # <--- Pseudo counts
-			if ($StrainClass{$Strain} eq $Class){
-				$FeatureClassHit = $TrainingMatrix[$i][$j];    # <---- Observed Hits
-            #$FeatureClassHit = $TrainingMatrix[$i][$j]+1;  # <---- Pseudo counts
-				$FeatureClass{$Feature}{$Class} += $FeatureClassHit; #Hits of probe in class
+			$Element = $TrainingMatrix[0][$j];
+         $TotalFeatureHits{$Feature} += $TrainingMatrix[$i][$j]+$PsCounts; # <- Total Feature Hits
+			if ($ClassOfElement{$Element} eq $Class){
+				$HitsOfFeaturesInClass{$Feature}{$Class} += $TrainingMatrix[$i][$j]+$PsCounts; # <- Total Feature Hits in Class
 			}
 		}
 	}
 }
 
-# Maximum Likelihood Estimation (MLE)
-$Report -> [0][0] = "Features";
+# Statistic tests
+$Report -> [0][0] = "Feature";
 $iClass = 1;
 for ($i=0; $i<$nClasses; $i++){
    $Class = $Classes[$i];
@@ -136,27 +151,28 @@ for ($i=0; $i<$nClasses; $i++){
    for ($j=1;$j<$LinesOnTrainingFile;$j++){
       $Feature = $TrainingMatrix[$j][0];
 
-      $a= (($FeatureClass{$Feature}{$Class}))+0.001; # hits de sonda a en clase a
-      $b= (($FeatureTotalHits{$Feature}-$FeatureClass{$Feature}{$Class}))+0.001; # Hits de sonda a que no están en clase A
-      $c= (($Classes{$Class}-$FeatureClass{$Feature}{$Class}))+0.001; # Numero de no hits en clase A (numero de ceros en clase A)
-      $d= ((($N-$Classes{$Class})-($FeatureTotalHits{$Feature}-$FeatureClass{$Feature}{$Class})))+0.001; # Numero de ceros fuera de A
-      
+      $a= (($HitsOfFeaturesInClass{$Feature}{$Class}))+0.001; # hits de sonda a en clase a
+      $b= (($TotalFeatureHits{$Feature}-$HitsOfFeaturesInClass{$Feature}{$Class}))+0.001; # Hits de sonda a que no están en clase A
+      $c= (($Elements{$Class}-$HitsOfFeaturesInClass{$Feature}{$Class}))+0.001; # Numero de mismaches en clase A (numero de ceros en clase A)
+      $d= ((($N-$Elements{$Class})-($TotalFeatureHits{$Feature}-$HitsOfFeaturesInClass{$Feature}{$Class})))+0.001; # Numero de ceros fuera de A
       $nConfusion = $a+$b+$c+$d;
       
-      
-      $MLE{$Feature} = (($a/$nConfusion)*(log2(($nConfusion*$a)/(($a+$b)*($a+$c)))))+
-                       (($b/$nConfusion)*(log2(($nConfusion*$b)/(($b+$a)*($b+$d)))))+
-                       (($c/$nConfusion)*(log2(($nConfusion*$c)/(($c+$d)*($c+$a)))))+
-                       (($d/$nConfusion)*(log2(($nConfusion*$d)/(($d+$c)*($d+$b)))));
-      
+      if ($MLE == 1){         # <------------------ Maximum Likelihood Estimation
+         $Test{$Feature} = (($a/$nConfusion)*(log2(($nConfusion*$a)/(($a+$b)*($a+$c)))))+
+                           (($b/$nConfusion)*(log2(($nConfusion*$b)/(($b+$a)*($b+$d)))))+
+                           (($c/$nConfusion)*(log2(($nConfusion*$c)/(($c+$d)*($c+$a)))))+
+                           (($d/$nConfusion)*(log2(($nConfusion*$d)/(($d+$c)*($d+$b))))); 
+      }elsif ($Chi2 == 1){    # <------------------------------------ Chi squared
+         $Test{$Feature} = (($nConfusion*(($a*$d)-($b*$c))**2))/(($a+$c)*($a+$b)*($b+$d)*($c+$d));
+      }
       $Report -> [$j][0] = $Feature;
-      $Report -> [$j][$iClass] = $MLE{$Feature};
+      $Report -> [$j][$iClass] = $Test{$Feature};
    }
    $iClass++;
 }
 
-#Building autput file
-open (FILE, ">$FeaturesMLE");
+# Building output file
+open (FILE, ">$TestReport");
 for ($i=0;$i<$LinesOnTrainingFile;$i++){
    for ($j=0;$j<5;$j++){
       print FILE $Report -> [$i][$j], ",";
@@ -165,29 +181,26 @@ for ($i=0;$i<$LinesOnTrainingFile;$i++){
 }
 close FILE;
 
-chdir ($MainPath);
-
-open (RSCRIPT, ">$RScript");
-        print RSCRIPT 'library(ggplot2)' . "\n";
-        print RSCRIPT "df <- read.csv(\"$FeaturesMLE\")" . "\n";
-        print RSCRIPT 'ggplot(df, aes(Features))';
-        foreach $Class(@Classes){
-         print RSCRIPT "+ geom_point(aes(y=$Class,color=\"$Class\"))";
-         #print RSCRIPT '+ geom_line(aes(y=PanGenome,linetype="PanGenome"))';
-         #print RSCRIPT '+ geom_line(aes(y=NewGenes,linetype="NewGenes"))';
-        }
-        #print RSCRIPT "+ scale_x_continuous(breaks = 0:$nClasses+1)";
-        print RSCRIPT "+ labs(x=\"Classes\", y=\"bits\", title= \"Maximum Likelihood Estimation\")";
-        #print RSCRIPT '+ scale_linetype_discrete(name=NULL)';
-        #print RSCRIPT '+ theme(axis.text.x = element_text(angle = 90, size=4, hjust = 1))' . "\n";
-        print RSCRIPT "\n";
-        print RSCRIPT "ggsave(\"$Plot\")";
+# Building plot
+print "\n Building Plot...";
+chdir($OutPath);
+open(RSCRIPT, ">$RScript");
+   print RSCRIPT 'library(ggplot2)' . "\n";
+   print RSCRIPT "df <- read.csv(\"$TestReport\")" . "\n";
+   print RSCRIPT 'ggplot(df, aes(Feature))';
+   foreach $Class(@Classes){
+      print RSCRIPT "+ geom_point(aes(y=$Class,color=\"$Class\"))";
+   }
+   print RSCRIPT "+ labs(x=\"Features\", y=\"bits\", title= \"$Test\", color=\"Class\")";
+   if($N > 100){
+      print RSCRIPT '+ theme(axis.text.x = element_text(angle = 90, size=4, hjust = 1))' . "\n";
+   }
+   print RSCRIPT "\n";
+   print RSCRIPT "ggsave(\"$Plot\")";
 close RSCRIPT;
 
-print "\n Building Plot...";
 system ("R CMD BATCH $RScript");
-print "Done!\n\n";
-
 system ("rm $RScript $MainPath/*.Rout $MainPath/Rplots.pdf");
+print "Done!\n\n";
 
 exit;
